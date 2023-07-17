dc72bad15092e0ce1b1152059b6a9c5ab4235aed
daniel <danielmaikath10@gmail.com>
2023-07-17 18:56:45 +0000

    Horizontal movement implemented

--- Code/player.js
+++ Code/player.js
@@ -2,10 +2,13 @@
 
 
 function Player(x,y,parent){
+    var self = this
     this.x = x ;
     this.y = y ;
+    this.direction = 0
     this.height = 50
     this.width = 50
+    this.speed = 2
     this.sprite = document.createElement("div")
 
     this.insertPlayer = function () { // Función encargada de insertar al player en el DOM
@@ -14,7 +17,13 @@ function Player(x,y,parent){
         this.sprite.style.top = this.y + 'px'
         parent.appendChild(this.sprite)
       }
-    
+    this.move = function(){ // Función encargada de mover al jugador horizontalmente
+      console.log(self.x)
+      self.x = self.x + self.speed * self.direction
+      if(self.x <= 450 && self.x >=0){
+      self.sprite.style.left = self.x + 'px' 
+    }
+    }
 
 }
 
--- Code/script.js
+++ Code/script.js
@@ -1,7 +1,32 @@
 import { Player } from "./player.js"
 const board = document.getElementById("board")
 const player = new Player(225,750,board)
-player.insertPlayer()
+
+
+
+window.addEventListener("keydown",function(e){
+    switch(e.key){
+        case "ArrowLeft": 
+            player.direction = -1
+            break;
+        case "ArrowRight":
+            player.direction = 1
+            break;
+    }
+
+
+})
+
+
+
+
+
+//Función que comienza el juego
+function start(){
+    player.insertPlayer()
+    let timerId = setInterval(player.move,16)
+}
+start()
 
 console.log(player)
 
